be00b73ac47cc7d7affaf328391c1326b1779f4d
Yimismi <1143937551@qq.com>
2018-02-16 19:54:51 +0800

    use jgrapht 1.1.1-SNAPSHOT

--- src/main/java/org/familytree/lcasdemo/Demo.java
+++ src/main/java/org/familytree/lcasdemo/Demo.java
@@ -1,6 +1,4 @@
 package org.familytree.lcasdemo;
-
-import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleDirectedGraph;
 import org.jgrapht.io.DOTImporter;
@@ -9,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import org.jgrapht.alg.NaiveLcaFinder;
 
 public class Demo {
     public static void main(String[] args) throws IOException, ImportException {
@@ -16,27 +15,28 @@ public class Demo {
         Scanner input = new Scanner(System.in);
         fileName = input.next();
         SimpleDirectedGraph<String, DefaultEdge> graph = buildGraph(fileName);
-        MyNaiveLcaFinder<String, DefaultEdge> lcaFinder = new MyNaiveLcaFinder<>(graph);
-		while (input.hasNext()) {
-			firstPersonName = input.next();
-        	secondPersonName = input.next();
-		    Set<String> lcas = lcaFinder.findLcas(firstPersonName, secondPersonName);
-			if ( lcas == null) {
-				System.out.println("The two men have no common ancestor");
-				continue;
-			}
-		    Iterator<String> it = lcas.iterator();
-		    while (it.hasNext()) {
-		        System.out.println(it.next());
-		    }
-		}
+        removeCircle(graph);
+        NaiveLcaFinder<String, DefaultEdge> lcaFinder = new NaiveLcaFinder<>(graph);
+        while (input.hasNext()) {
+            firstPersonName = input.next();
+            secondPersonName = input.next();
+            Set<String> lcas = lcaFinder.findLcas(firstPersonName, secondPersonName);
+            if ( lcas == null) {
+                System.out.println("The two men have no common ancestor");
+                continue;
+            }
+            Iterator<String> it = lcas.iterator();
+            while (it.hasNext()) {
+                System.out.println(it.next());
+            }
+        }
     }
 
 
     /*
      * Import graph from .dot file
      */
-    static private SimpleDirectedGraph buildGraph(String fileName) throws IOException, ImportException {
+    private static SimpleDirectedGraph buildGraph(String fileName) throws IOException, ImportException {
         SimpleDirectedGraph<String, DefaultEdge> graph = new SimpleDirectedGraph<>(DefaultEdge.class);
         DOTImporter<String, DefaultEdge> importer = new DOTImporter((id, attributes) -> {
             return id;
@@ -54,54 +54,22 @@ public class Demo {
         }
         return graph;
     }
-}
-class MyNaiveLcaFinder<V, E> {
-    private Graph<V, E> graph = null;
-    public MyNaiveLcaFinder(Graph graph) {
-        this.graph = graph;
-    }
-    public Set<V> findLcas(V a, V b) {
-        return this.findLcas(Collections.singleton(a), Collections.singleton(b), new LinkedHashSet(), new LinkedHashSet());
-    }
-    private Set<V> findLcas(Set<V> aSet, Set<V> bSet, LinkedHashSet<V> aSeenSet, LinkedHashSet<V> bSeenSet) {
-        if (aSet.size() == 0 && bSet.size() == 0) {
-            return null;
-        } else if (!Collections.disjoint(aSet, bSeenSet)) {
-            return this.overlappingMember(aSet, bSeenSet);
-        } else if (!Collections.disjoint(bSet, aSeenSet)) {
-            return this.overlappingMember(bSet, aSeenSet);
-        } else if (!Collections.disjoint(aSet, bSet)) {
-            return this.overlappingMember(aSet, bSet);
-        } else {
-            aSeenSet.addAll(aSet);
-            bSeenSet.addAll(bSet);
-            aSet = this.allParents(aSet);
-            aSet.removeAll(aSeenSet);
-            bSet = this.allParents(bSet);
-            bSet.removeAll(bSeenSet);
-            return this.findLcas(aSet, bSet, aSeenSet, bSeenSet);
-        }
-    }
-    private Set<V> allParents(Set<V> vertexSet) {
-        HashSet<V> result = new HashSet();
-        Iterator<V> var3 = vertexSet.iterator();
 
-        while(var3.hasNext()) {
-            V e = var3.next();
-            Iterator<E> var5 = this.graph.incomingEdgesOf(e).iterator();
-
-            while(var5.hasNext()) {
-                E edge = var5.next();
-                if (this.graph.getEdgeTarget(edge).equals(e)) {
-                    result.add(this.graph.getEdgeSource(edge));
+    /*
+     * Remove conjugal relation or potential conjugal relationship
+     */
+    private static void removeCircle(SimpleDirectedGraph graph) {
+        Object[] vectexs = graph.vertexSet().toArray();
+        for(int i = 0; i < vectexs.length - 1; i++) {
+            for(int j = i + 1; j < vectexs.length; j++) {
+                if(graph.containsEdge(vectexs[i], vectexs[j]) && graph.containsEdge(vectexs[j], vectexs[i])) {
+                    System.out.println(vectexs[i] + ": " + vectexs[j]);
+                    graph.removeEdge(vectexs[i], vectexs[j]);
+                    graph.removeEdge(vectexs[j], vectexs[i]);
                 }
             }
         }
+    }
 
-        return result;
-    }
-    private Set<V> overlappingMember(Set<V> x, Set<V> y) {
-        y.retainAll(x);
-        return y;
-    }
 }
+
